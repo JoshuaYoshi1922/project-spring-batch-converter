a78ed6eeb03194cae66da52ea99efe65fd32a8c9
BryJime <bry.jimenex@gmail.com>
2026-02-13 13:33:11 -0800

    Initial Commit

---
+++ spring-batch-file-converter/src/main/java/com/example/spring_batch_file_converter/SpringBatchFileConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_file_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFileConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchFileConverterApplication.class, args);
+	}
+
+}
